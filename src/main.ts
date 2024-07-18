8293a7cf80f7a68eeb8cb9a86d503683d1cc96ee
jiahao <2077780946@qq.com>
2024-07-18 11:26:51 +0800

    feat: roadmap 接口

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.setGlobalPrefix('/api')
+  app.enableCors({
+    origin: true,
+    methods: 'GET,PUT,POST',
+    // allowedHeaders: 'Content-Type,Authorization',
+    // exposedHeaders: 'Content-Range,X-Content-Range',
+    credentials: true,
+    maxAge: 4800,
+  });
   await app.listen(8080);
 }
 bootstrap();
